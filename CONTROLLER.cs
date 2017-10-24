3b22dbacbec34b4995fa74a519b910d4853133f1
wiggl3 <santana.igor18@gmail.com>
2017-10-24 20:14:29 -0200

    Add files via upload

---
+++ CONTROLLER.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CONTROLLER : MonoBehaviour {
+
+	public int cont = 0;
+	public string next;
+	public string after;
+
+	// Use this for initialization
+	void Awake()
+	{
+		DontDestroyOnLoad (transform.gameObject);
+	}
+
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		after = SceneManager.GetActiveScene().name.ToString();
+
+		if (next != after) 
+		{
+			cont += 1;
+			next = after;
+		}
+
+	}
+}
---
+++ Sinais.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sinais : MonoBehaviour {
+
+	SpriteRenderer changeColor;
+	bool vaiSatanas = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		changeColor = GetComponentInChildren<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (vaiSatanas == true) 
+		{
+			changeColor.color = Color.red;
+		}
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			if (Input.GetKeyDown (KeyCode.E)) 
+			{
+				vaiSatanas = true;
+			}
+		}
+	}
+}
