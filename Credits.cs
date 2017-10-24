89bf00fd6cb59cf2e2003c93f52e1d1344abae44
wiggl3 <santana.igor18@gmail.com>
2017-10-24 20:23:51 -0200

    Add files via upload

---
+++ Credits.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+	[SerializeField]Scrollbar names;
+	[SerializeField]Button back;
+
+	// Use this for initialization
+	void Start () {
+		back.onClick = new Button.ButtonClickedEvent();
+		back.onClick.AddListener(() => BackToGame());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		names.value -= Time.deltaTime/9;
+	}
+
+	void BackToGame()
+	{
+		SceneManager.LoadScene ("MenuInicial");
+	}
+
+}
