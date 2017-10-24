c923b41a79dfe8e7596dd11b5f6ea31b41121961
wiggl3 <santana.igor18@gmail.com>
2017-10-24 20:20:10 -0200

    Update SELECAO.cs

--- SELECAO.cs
+++ SELECAO.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,18 +6,15 @@ public class SELECAO : MonoBehaviour {
 
 	[SerializeField]
 	Transform[] planetsCam;
-	[Space(20)]
 	[SerializeField]
 	GameObject [] planets;
-	[Space(20)]
 	[SerializeField]
 	GameObject moldura;
-	[Space(20)]
 	[SerializeField]
 	GameObject fundo;
-	[Space(20)]
 	[SerializeField]
-	Button playLua;
+	Button playLua, information;
+	[SerializeField]Text planetName;
 
 	public int posicao = 0;
 	int quant;
@@ -36,34 +33,88 @@ public class SELECAO : MonoBehaviour {
 		playLua.onClick = new Button.ButtonClickedEvent ();
 		playLua.onClick.AddListener (() => playMoon());
 
+		information.onClick = new Button.ButtonClickedEvent ();
+		information.onClick.AddListener (() => Informations());
+
+		planetName.text = planetName.ToString ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown(KeyCode.D)) 
+		if (Input.GetKeyDown(KeyCode.RightArrow)) 
 		{
 			posicao++;
 			if(posicao > 7)
 				posicao = 0;
 		}
 
-		if (Input.GetKeyDown(KeyCode.A)) 
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) 
 		{
 			posicao--;
 			if(posicao < 0)
 				posicao = 7;
 		}
 
-		if (posicao == 2 && Input.GetKey(KeyCode.Return)) {
-			fundo.SetActive (true);
+		if (posicao == 0)
+		{
+			planetName.text = "Mercúrio";
 		} 
+
+
+		else if (posicao == 1)
+		{
+			planetName.text = "Venus";
+		} 
+
+		else if (posicao == 2)
+		{
+			planetName.text = "Terra (Lua)";
+		} 
+
+		else if (posicao == 3)
+		{
+			planetName.text = "Marte";
+		}
+
+		else if (posicao == 4)
+		{
+			planetName.text = "Jupiter";
+		}
+
+		else if (posicao == 5)
+		{
+			planetName.text = "Sarturno";
+		}
+
+		else if (posicao == 6)
+		{
+			planetName.text = "Urano";
+		}
+
+		else if (posicao == 7)
+		{
+			planetName.text = "Netuno";
+		}
 
 		moldura.transform.position = Vector3.Slerp(moldura.transform.position, planetsCam[posicao].position, 5*Time.deltaTime);
 	}
 
 	void playMoon()
 	{
-		SceneManager.LoadScene("Loading");
+		if (posicao == 2)
+		{
+			SceneManager.LoadScene ("Loading 1");
+		}
+	}
+
+	void Informations()
+	{
+
+		if (posicao == 2) 
+		{
+			fundo.SetActive (true);
+		}
 	}
 }
